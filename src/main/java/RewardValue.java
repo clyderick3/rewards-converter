9e611bd6a04e002775de286ec7a2dfcdcf33585e
Clyde Broderick <Clyde@returnofthemac.myfiosgateway.com>
2023-10-02 15:10:23 -0400

    Cleaned up RewardValue.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -3,26 +3,22 @@ public class RewardValue {
     public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
 
-    //Two constructors
     //Constructor that accepts cash.
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
     //Constructor that accepts miles.
-    //public RewardValue(double milesValue) {}
+    public RewardValue(int milesValue){
+        this.cashValue = convertToCash(milesValue);
+    }
 
     private static double convertToCash(int milesValue) {
         return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
-    public RewardValue(int milesValue){
-        this.cashValue = convertToCash(milesValue);
-    }
-
     private static int convertToMiles(double cashValue){
         return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
     }
-
 
     //getCashValue Method
     public double getCashValue() {
@@ -33,5 +29,4 @@ public class RewardValue {
     public int getMilesValue(){
         return convertToMiles(this.cashValue);
     }
-    //Miles to cash at a rate of 0.0035.
 }
